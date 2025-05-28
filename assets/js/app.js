00565e7fb639cc9cb3d10bdacfbe55d76556675e
songheewon <songheew1020@gmail.com>
2025-05-28 13:26:19 +0900

    feat: 수원테마 버전2 init

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,5 @@
-$(document).ready(function() {
-
-  'use strict';
+$(document).ready(function () {
+  "use strict";
 
   // =====================
   // Membership Navigation
@@ -10,32 +9,34 @@ $(document).ready(function() {
 
   var pathname = window.location.pathname;
 
-  $('.js-nav__link-membership[href="'+ pathname +'"]').addClass('c-nav__link--current');
+  $('.js-nav__link-membership[href="' + pathname + '"]').addClass(
+    "c-nav__link--current"
+  );
 
   // =====================
   // Mobile Drawer
   // =====================
 
-  $('.js-nav-toggle').click(function(e) {
+  $(".js-nav-toggle").click(function (e) {
     e.preventDefault();
-    $('.c-header-drawer').toggleClass('is-active');
+    $(".c-header-drawer").toggleClass("is-active");
 
     // $(this).toggleClass('c-nav-toggle--close');
 
-    $('body').toggleClass('e-mode-mobile');
+    $("body").toggleClass("e-mode-mobile");
   });
 
   // =====================
   // Koenig Gallery
   // =====================
-  var gallery_images = document.querySelectorAll('.kg-gallery-image img');
+  var gallery_images = document.querySelectorAll(".kg-gallery-image img");
 
   gallery_images.forEach(function (image) {
-    var container = image.closest('.kg-gallery-image');
+    var container = image.closest(".kg-gallery-image");
     var width = image.attributes.width.value;
     var height = image.attributes.height.value;
     var ratio = width / height;
-    container.style.flex = ratio + ' 1 0%';
+    container.style.flex = ratio + " 1 0%";
   });
 
   // =====================
@@ -45,50 +46,53 @@ $(document).ready(function() {
   // =====================
 
   function decoding_translation_chars(string) {
-    return $('<textarea/>').html(string).text();
+    return $("<textarea/>").html(string).text();
   }
 
   // =====================
   // Responsive videos
   // =====================
 
-  $('.c-content').fitVids({
-    'customSelector': [ 'iframe[src*="ted.com"]'          ,
-                        'iframe[src*="player.twitch.tv"]' ,
-                        'iframe[src*="dailymotion.com"]'  ,
-                        'iframe[src*="facebook.com"]'
-                      ]
+  $(".c-content").fitVids({
+    customSelector: [
+      'iframe[src*="ted.com"]',
+      'iframe[src*="player.twitch.tv"]',
+      'iframe[src*="dailymotion.com"]',
+      'iframe[src*="facebook.com"]',
+    ],
   });
 
   // =====================
   // Responsive Table
   // =====================
-  $('.post-template .c-content > table').wrap('<div class="c-content__table--wrap">');
+  $(".post-template .c-content > table").wrap(
+    '<div class="c-content__table--wrap">'
+  );
 
   // =====================
   // Images zoom
   // =====================
 
-  $('.c-content img').attr('data-action', 'zoom');
+  $(".c-content img").attr("data-action", "zoom");
 
   // If the image is inside a link, remove zoom
-  $('.c-content a img').removeAttr('data-action');
+  $(".c-content a img").removeAttr("data-action");
 
   // =====================
   // Clipboard URL Copy
   // =====================
 
-  var clipboard = new ClipboardJS('.js-share__link--clipboard');
+  var clipboard = new ClipboardJS(".js-share__link--clipboard");
 
-  clipboard.on('success', function(e) {
+  clipboard.on("success", function (e) {
     var element = $(e.trigger);
 
-    element.addClass('tooltipped tooltipped-s');
-    element.attr('aria-label', clipboard_copied_text);
+    element.addClass("tooltipped tooltipped-s");
+    element.attr("aria-label", clipboard_copied_text);
 
-    element.mouseleave(function() {
-      $(this).removeAttr('aria-label');
-      $(this).removeClass('tooltipped tooltipped-s');
+    element.mouseleave(function () {
+      $(this).removeAttr("aria-label");
+      $(this).removeClass("tooltipped tooltipped-s");
     });
   });
 
@@ -96,10 +100,11 @@ $(document).ready(function() {
   // Search
   // =====================
 
-  var search_field = $('.js-search-input'),
-      search_results = $('.js-search-results'),
-      toggle_search = $('.js-search-toggle'),
-      search_result_template = "\
+  var search_field = $(".js-search-input"),
+    search_results = $(".js-search-results"),
+    toggle_search = $(".js-search-toggle"),
+    search_result_template =
+      "\
       <a href={{link}} class='c-search-result'>\
         <div class='c-search-result__content'>\
           <h3 class='c-search-result__title'>{{title}}</h3>\
@@ -110,71 +115,196 @@ $(document).ready(function() {
         </div>\
       </a>";
 
-  toggle_search.click(function(e) {
+  toggle_search.click(function (e) {
     e.preventDefault();
-    $('.js-search').addClass('is-active');
+    $(".js-search").addClass("is-active");
 
     // If off-canvas is active, just disable it
-    $('.js-off-canvas-container').removeClass('is-active');
+    $(".js-off-canvas-container").removeClass("is-active");
 
-    setTimeout(function() {
+    setTimeout(function () {
       search_field.focus();
     }, 500);
   });
 
-  $('.c-search, .js-search-close, .js-search-close .icon').on('click keyup', function(event) {
-    if (event.target == this || event.target.className == 'js-search-close' || event.target.className == 'icon' || event.keyCode == 27) {
-      $('.c-search').removeClass('is-active');
+  $(".c-search, .js-search-close, .js-search-close .icon").on(
+    "click keyup",
+    function (event) {
+      if (
+        event.target == this ||
+        event.target.className == "js-search-close" ||
+        event.target.className == "icon" ||
+        event.keyCode == 27
+      ) {
+        $(".c-search").removeClass("is-active");
+      }
     }
-  });
+  );
 
   search_field.ghostHunter({
     results: search_results,
-    onKeyUp         : true,
-    result_template : search_result_template,
-    zeroResultsInfo : false,
+    onKeyUp: true,
+    result_template: search_result_template,
+    zeroResultsInfo: false,
     displaySearchInfo: false,
-    before: function() {
+    before: function () {
       search_results.fadeIn();
-    }
+    },
   });
 
   // =====================
   // Ajax Load More
   // =====================
 
-  var pagination_next_url = $('link[rel=next]').attr('href'),
-    $load_posts_button = $('.js-load-posts');
-
-  $load_posts_button.click(function(e) {
+  var pagination_next_url = $("link[rel=next]").attr("href"),
+    $load_posts_button = $(".js-load-posts");
+    var pagination_next_page_number = 1
+    $load_posts_button.click(function (e) {
     e.preventDefault();
-
     var request_next_link =
       pagination_next_url.split(/page/)[0] +
-      'page/' +
-      pagination_next_page_number +
-      '/';
-
+      "page/" +
+      ++pagination_next_page_number +
+      "/";
     $.ajax({
       url: request_next_link,
-      beforeSend: function() {
-        $load_posts_button.text(decoding_translation_chars(pagination_loading_text));
-        $load_posts_button.addClass('c-btn--loading');
-      }
-    }).done(function(data) {
-      var posts = $('.js-post-card__wrap', data);
+      beforeSend: function () {
+        
+      },
+    }).done(function (data) {
+      var posts = $(".js-post-card__wrap", data);
+      $(".js-grid").append(posts);
 
-      $('.js-grid').append(posts);
-
-      $load_posts_button.text(decoding_translation_chars(pagination_more_posts_text));
-      $load_posts_button.removeClass('c-btn--loading');
-
-      pagination_next_page_number++;
+      $load_posts_button.text(
+        decoding_translation_chars(pagination_more_posts_text)
+      );
+      $load_posts_button.removeClass("c-btn--loading");
 
       // If you are on the last pagination page, hide the load more button
-      if (pagination_next_page_number > pagination_available_pages_number) {
-        $load_posts_button.addClass('c-btn--disabled').attr('disabled', true);
+      if (pagination_next_page_number+1 > pagination_available_pages_number) {
+        $load_posts_button.addClass("c-btn--disabled").attr("disabled", true);
       }
+    })
+  });
+
+  const all = document.getElementById('load-all');
+  const letter = document.getElementById('load-letter');
+  const book = document.getElementById('load-book');
+  const article = document.getElementById('load-article');
+
+  var $load_all_button = $(".js-load-all");
+  $load_all_button.click(function (e) {
+    e.preventDefault();
+    letter.style.color = "#979797";
+    letter.style.backgroundColor = "#E9E9E9";
+    all.style.color = "#FFFFFF";
+    all.style.backgroundColor = "#C0C0C0";
+    book.style.color = "#979797";
+    book.style.backgroundColor = "#E9E9E9"
+    article.style.color = "#979797";
+    article.style.backgroundColor = "#E9E9E9"
+
+    pagination_next_url = "/";
+    pagination_next_page_number = 0;
+    var request_next_link =
+      pagination_next_url.split(/page/)[0] +
+      "page/" +
+      ++pagination_next_page_number +
+      "/";
+    $.ajax({
+      url: request_next_link,
+    }).done(function (data) {
+      var posts = $(".js-post-card__wrap", data);
+
+      $(".js-grid").empty();
+      $(".js-grid").append(posts);
+    });
+  });
+
+  var $load_letter_button = $(".js-load-letter");
+  $load_letter_button.click(function (e) {
+    e.preventDefault();
+    all.style.color = "#979797";
+    all.style.backgroundColor = "#E9E9E9";
+    letter.style.color = "#FFFFFF";
+    letter.style.backgroundColor = "#C0C0C0";
+    book.style.color = "#979797";
+    book.style.backgroundColor = "#E9E9E9"
+    article.style.color = "#979797";
+    article.style.backgroundColor = "#E9E9E9"
+
+    pagination_next_url = "/tag/doingletter/";
+    pagination_next_page_number = 0;
+    var request_next_link =
+      pagination_next_url.split(/page/)[0] +
+      "page/" +
+      ++pagination_next_page_number +
+      "/";
+    $.ajax({
+      url: request_next_link,
+    }).done(function (data) {
+      var posts = $(".js-post-card__wrap", data);
+
+      $(".js-grid").empty();
+      $(".js-grid").append(posts);
+    });
+  });
+
+  var $load_book_button = $(".js-load-book");
+  $load_book_button.click(function (e) {
+    e.preventDefault();
+
+    all.style.color = "#979797";
+    all.style.backgroundColor = "#E9E9E9";
+    book.style.color = "#FFFFFF";
+    book.style.backgroundColor = "#C0C0C0";
+    letter.style.color = "#979797";
+    letter.style.backgroundColor = "#E9E9E9"
+    article.style.color = "#979797";
+    article.style.backgroundColor = "#E9E9E9"
+
+    pagination_next_url = "/shop/category/book/";
+    pagination_next_page_number = 0;
+    var request_next_link =
+      pagination_next_url.split(/page/)[0] +
+      "page/" +
+      ++pagination_next_page_number +
+      "/";
+    $.ajax({
+      url: request_next_link,
+    }).done(function (data) {
+      var posts = $(".js-content-card", data);
+
+      $(".js-grid").empty();
+      $(".js-grid").append(posts);
+    });
+  });
+
+  var $load_article_button = $(".js-load-article");
+  $load_article_button.click(function (e) {
+    e.preventDefault();
+    all.style.color = "#979797";
+    all.style.backgroundColor = "#E9E9E9";
+    article.style.color = "#FFFFFF";
+    article.style.backgroundColor = "#C0C0C0";
+    book.style.color = "#979797";
+    book.style.backgroundColor = "#E9E9E9"
+    letter.style.color = "#979797";
+    letter.style.backgroundColor = "#E9E9E9"
+    pagination_next_url = "/tag/article/";
+    pagination_next_page_number = 0;
+    var request_next_link =
+      pagination_next_url.split(/page/)[0] +
+      "page/" +
+      ++pagination_next_page_number +
+      "/";
+    $.ajax({
+      url: request_next_link,
+    }).done(function (data) {
+      var posts = $(".js-post-card__wrap", data);
+
+      $(".js-grid").empty();
+      $(".js-grid").append(posts);
     });
   });
 });
@@ -184,89 +314,133 @@ $(document).ready(function() {
  */
 
 function toggleShareDropdown() {
-  var shareIcons = document.getElementById('share-icons');
-  shareIcons.classList.toggle('show');
+  var shareIcons = document.getElementById("share-icons");
+  shareIcons.classList.toggle("show");
 }
 
 function shareOnFacebook() {
   var sharedURL = location.href;
-  var facebookShareURL = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(sharedURL);
-  window.open(facebookShareURL, '_blank');
+  var facebookShareURL =
+    "https://www.facebook.com/sharer/sharer.php?u=" +
+    encodeURIComponent(sharedURL);
+  window.open(facebookShareURL, "_blank");
 }
 
 function shareOnTwitter() {
   var sharedURL = location.href;
-  var twitterShareURL = 'https://twitter.com/intent/tweet?url=' + encodeURIComponent(sharedURL);
-  window.open(twitterShareURL, '_blank');
+  var twitterShareURL =
+    "https://twitter.com/intent/tweet?url=" + encodeURIComponent(sharedURL);
+  window.open(twitterShareURL, "_blank");
 }
 
 function shareOnPinterest() {
   var sharedURL = location.href;
-  var pinterestShareURL = 'https://www.pinterest.com/pin/create/button/?url=' + encodeURIComponent(sharedURL);
-  window.open(pinterestShareURL, '_blank');
+  var pinterestShareURL =
+    "https://www.pinterest.com/pin/create/button/?url=" +
+    encodeURIComponent(sharedURL);
+  window.open(pinterestShareURL, "_blank");
 }
 
 function shareOnLinkedIn() {
   var sharedURL = location.href;
-  var linkedInShareURL = 'https://www.linkedin.com/sharing/share-offsite/?url=' + encodeURIComponent(sharedURL);
-  window.open(linkedInShareURL, '_blank');
+  var linkedInShareURL =
+    "https://www.linkedin.com/sharing/share-offsite/?url=" +
+    encodeURIComponent(sharedURL);
+  window.open(linkedInShareURL, "_blank");
 }
 
 function shareOnFlipboard() {
   var sharedURL = location.href;
-  var flipboardShareURL = 'https://share.flipboard.com/bookmarklet/popout?v=2&title=&url=' + encodeURIComponent(sharedURL);
-  window.open(flipboardShareURL, '_blank');
+  var flipboardShareURL =
+    "https://share.flipboard.com/bookmarklet/popout?v=2&title=&url=" +
+    encodeURIComponent(sharedURL);
+  window.open(flipboardShareURL, "_blank");
 }
 
 function shareOnTelegram() {
   var sharedURL = location.href;
-  var telegramShareURL = 'https://telegram.me/share/url?url=' + encodeURIComponent(sharedURL);
-  window.open(telegramShareURL, '_blank');
+  var telegramShareURL =
+    "https://telegram.me/share/url?url=" + encodeURIComponent(sharedURL);
+  window.open(telegramShareURL, "_blank");
 }
 
 function shareOnWhatsApp() {
   var sharedURL = location.href;
-  var whatsAppShareURL = 'whatsapp://send?text=' + encodeURIComponent(sharedURL);
-  window.open(whatsAppShareURL, '_blank');
+  var whatsAppShareURL =
+    "whatsapp://send?text=" + encodeURIComponent(sharedURL);
+  window.open(whatsAppShareURL, "_blank");
 }
 
 function shareOnReddit() {
   var sharedURL = location.href;
-  var redditShareURL = 'https://www.reddit.com/submit?url=' + encodeURIComponent(sharedURL);
-  window.open(redditShareURL, '_blank');
+  var redditShareURL =
+    "https://www.reddit.com/submit?url=" + encodeURIComponent(sharedURL);
+  window.open(redditShareURL, "_blank");
 }
 
 function shareViaEmail() {
   var sharedURL = location.href;
-  var emailSubject = 'Check out this link';
-  var emailBody = 'I found this interesting link and thought you might like it: ' + sharedURL;
-  var mailToLink = 'mailto:?subject=' + encodeURIComponent(emailSubject) + '&body=' + encodeURIComponent(emailBody);
+  var emailSubject = "Check out this link";
+  var emailBody =
+    "I found this interesting link and thought you might like it: " + sharedURL;
+  var mailToLink =
+    "mailto:?subject=" +
+    encodeURIComponent(emailSubject) +
+    "&body=" +
+    encodeURIComponent(emailBody);
   window.location.href = mailToLink;
 }
 
 function shareNative() {
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
     // Mobile device, trigger native sharing
     var sharedURL = location.href;
-    navigator.share({ url: sharedURL })
-      .then(() => console.log('Shared successfully.'))
-      .catch((error) => console.log('Error sharing:', error));
+    navigator
+      .share({ url: sharedURL })
+      .then(() => console.log("Shared successfully."))
+      .catch((error) => console.log("Error sharing:", error));
   } else {
-    console.log('Native sharing not supported on this device.');
+    console.log("Native sharing not supported on this device.");
   }
 }
 
 function copyCurrentPageURL() {
-  navigator.clipboard.writeText(window.location.href).then(
-    () => {
-      alert('URL이 복사되었습니다.');
-    }
-  );
+  navigator.clipboard.writeText(window.location.href).then(() => {
+    alert("URL이 복사되었습니다.");
+  });
 }
 
-var searchForm = document.getElementById('c-search-form');
-  searchForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    var searchParam = $(this).find('.c-search__stx')[0].value;
-    window.location.href = '/search/' + encodeURIComponent(searchParam.trim());
+/**
+ * Products Carousel
+ */
+const productsCarousel = document.querySelectorAll(
+  ".s-products-carousel__container"
+);
+productsCarousel.forEach((el) => {
+  const swiper = new Swiper(el.querySelector(".swiper"), {
+    spaceBetween: 20,
+    slidesPerView: 2,
+    breakpoints: {
+      640: {
+        spaceBetween: 40,
+      },
+      1024: {
+        spaceBetween: 20,
+        slidesPerView: 3,
+      },
+    },
+    pagination: {
+      el: el.querySelector(".swiper-pagination"),
+      clickable: true,
+    },
+    navigation: {
+      nextEl: el.querySelector(".swiper-button-next"),
+      prevEl: el.querySelector(".swiper-button-prev"),
+    },
+    watchSlidesProgress: true,
   });
+});
